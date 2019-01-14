24f679e6abd402807f3cae608cebab624424ac06
Sergey Bubnov <omg@altlinux.org>
2019-01-14 16:01:47 +0400

    first naive LDAP browsing with attrubutes

---
+++ adfs.py
@@ -0,0 +1,355 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, sys, stat, errno, signal, logging, pyfuse3, trio
+import stat
+from time import time
+import sqlite3
+from argparse import ArgumentParser
+from adldap import AD
+import ldif
+
+try:
+    import faulthandler
+except ImportError:
+    pass
+else:
+    faulthandler.enable()
+
+
+#trio_SIGINT_handler = signal.getsignal(signal.SIGINT)
+#def keyboardInterruptHandler(signo, frame):
+#    print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signo))
+#    trio_SIGINT_handler(signo, frame)
+#    exit(0)
+
+#signal.signal(signal.SIGINT, keyboardInterruptHandler)
+log = logging.getLogger(__name__)
+
+def_dir_mode = stat.S_IFDIR | stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH
+
+def r2dn(r):
+    return ','.join(['DC=%s' % x for x in r.lower().split('.')])
+
+class ADfs(pyfuse3.Operations):
+    def path2dn(self, path):
+        return (self.realm)
+
+
+    def __init__(self):
+        super(ADfs, self).__init__()
+        self.realm = 'domain.alt'
+        self.ad = AD(self.realm)
+        self.cwd = r2dn(self.realm)
+
+        #init sqlite
+        self.db = sqlite3.connect(':memory:')
+        self.db.text_factory = str
+        self.db.row_factory = sqlite3.Row
+        self.cursor = self.db.cursor()
+        self.init_tables()
+        self.update_node('/')
+
+
+    def _update_root(self):
+        nodes = self.ad.get_childs(r2dn(self.realm))
+        for _n in nodes:
+            dn = str.encode(_n)[:-len(r2dn(self.realm))-1] # DN without realm's DN
+            cursor2 = self.db.cursor()
+            inode = cursor2.execute("SELECT inode FROM contents WHERE name=?", (dn,)).fetchone()
+            if inode: # node already exists
+                log.debug("skip inode: %s" % inode)
+                break
+            else:
+                log.debug("add %s to /" % dn)
+                now_ns = int(time() * 1e9)
+                self.cursor.execute("INSERT INTO inodes (uid, gid, mode, mtime_ns, atime_ns, ctime_ns, target, rdev) "
+                                    "VALUES (?,?,?,?,?,?,?,?)",
+                                    (os.getuid(), os.getgid(), stat.S_IFDIR | 0o755, now_ns, now_ns, now_ns, None, 0))
+                inode = self.cursor.lastrowid
+                self.db.execute("INSERT INTO contents (name, inode, parent_inode) VALUES (?,?,?)", (dn, inode, pyfuse3.ROOT_INODE))
+
+
+    def get_inode_name(self, inode):
+        return self.get_row("SELECT name FROM contents WHERE inode=?", (inode,))['name']
+
+
+    def get_inode_path(self, inode):
+        path = []
+        pinode = inode
+        while pinode != pyfuse3.ROOT_INODE:
+            name = self.get_inode_name(pinode)
+            path.append(name.decode("utf-8"))
+            log.debug("adding %s to a path" % name)
+            pinode = self.get_row("SELECT parent_inode FROM contents WHERE inode=?", (pinode,))['parent_inode']
+
+        return ('/' + '/'.join(path))
+#        return ('/' + '/'.join(reversed(path)))
+
+
+    def path2dn(self, path):
+        log.debug('path2dn: %s' % path)
+        p = ','.join(path.split('/')[1:])
+        res = p + ',' + r2dn(self.realm)
+        log.debug('path2dn (res): %s' % res)
+        return res
+
+
+    def update_node(self, node):
+        if node == '/': # update root (realm) node
+            self._update_root()
+
+
+    def update_inode(self, inode):
+        if inode == pyfuse3.ROOT_INODE:
+            self._update_root()
+            return
+
+        # try to get children from cache
+        childs = self.db.execute("SELECT * FROM contents WHERE parent_inode=?", (inode,)).fetchone()
+        if not childs: # update cache from AD
+            cwd = self.get_inode_path(inode)
+            log.debug('cwd: %s' % cwd)
+            pdn = self.path2dn(cwd) # parent dn
+            log.debug('pdn: %s' % pdn)
+            dns = self.ad.get_childs(pdn)
+            now_ns = int(time() * 1e9)
+
+            # insert attributes file
+            log.debug("add attributes to %s" % pdn)
+            name = b'.attributes'
+            cursor2 = self.db.cursor()
+            cursor2.execute("INSERT INTO inodes (uid, gid, mode, mtime_ns, atime_ns, ctime_ns, target, rdev) "
+                            "VALUES (?,?,?,?,?,?,?,?)",
+                            (os.getuid(), os.getgid(), stat.S_IFREG | 0o644, now_ns, now_ns, now_ns, None, 0))
+            attrs_inode = cursor2.lastrowid
+            log.debug("insert new record: (name, inode, parent_inode) (%s, %s, %s)" % (name, attrs_inode, inode))
+            self.db.execute("INSERT INTO contents (name, inode, parent_inode) VALUES (?,?,?)", (name, attrs_inode, inode))
+            data_len = self.update_inode_data(attrs_inode)
+            self.db.execute("UPDATE inodes SET size=? WHERE id=?", (data_len, attrs_inode))
+
+
+            for dn in dns: # insert node to the cache
+                log.debug("add %s to %s" % (pdn, dn))
+                name = str.encode(dn)[:-len(pdn)-1]
+                self.cursor.execute("INSERT INTO inodes (uid, gid, mode, mtime_ns, atime_ns, ctime_ns, target, rdev) "
+                                    "VALUES (?,?,?,?,?,?,?,?)",
+                                    (os.getuid(), os.getgid(), stat.S_IFDIR | 0o755, now_ns, now_ns, now_ns, None, 0))
+                c_inode = self.cursor.lastrowid
+                log.debug("insert new record: (name, inode, parent_inode) (%s, %s, %s)" % (name, c_inode, inode))
+                self.db.execute("INSERT INTO contents (name, inode, parent_inode) VALUES (?,?,?)", (name, c_inode, inode))
+        else:
+            log.debug("skip updating %s" % self.get_inode_name(inode))
+
+
+    def init_tables(self):
+        self.cursor.execute("""
+        CREATE TABLE inodes (
+          id             INTEGER PRIMARY KEY,
+          uid            INT NOT NULL,
+          gid            INT NOT NULL,
+          mode           INT NOT NULL,
+          mtime_ns       INT NOT NULL,
+          atime_ns       INT NOT NULL,
+          ctime_ns       INT NOT NULL,
+          target         BLOB(256),
+          size           INT NOT NULL DEFAULT 0,
+          rdev           INT NOT NULL DEFAULT 0,
+          data           BLOB
+        )""")
+
+        self.cursor.execute("""
+        CREATE TABLE contents (
+          rowid          INTEGER PRIMARY KEY AUTOINCREMENT,
+          name           BLOB(256) NOT NULL,
+          inode          INT NOT NULL REFERENCES inodes(id),
+          parent_inode   INT NOT NULL REFERENCES inodes(id),
+
+          UNIQUE (name, parent_inode)
+        )""")
+
+        # insert root dir
+        now_ns = int(time() * 1e9)
+        self.cursor.execute("INSERT INTO inodes (id,mode,uid,gid,mtime_ns,atime_ns,ctime_ns) "
+                            "VALUES (?,?,?,?,?,?,?) ",
+                            (pyfuse3.ROOT_INODE, stat.S_IFDIR |
+                             stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
+                             stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
+                             stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH ,
+                             os.getuid(), os.getgid(), now_ns, now_ns, now_ns))
+
+        self.cursor.execute("INSERT INTO contents (name, parent_inode, inode) VALUES(?,?,?)",
+                            (b'..', pyfuse3.ROOT_INODE, pyfuse3.ROOT_INODE))
+
+        self.cursor.execute("INSERT INTO inodes (mode,uid,gid,mtime_ns,atime_ns,ctime_ns) "
+                            "VALUES (?,?,?,?,?,?) ",
+                            (stat.S_IFDIR | 0o755,
+                             os.getuid(), os.getgid(), now_ns, now_ns, now_ns))
+        cid = self.cursor.lastrowid
+        self.cursor.execute("INSERT INTO contents (name, parent_inode, inode) VALUES(?,?,?)",
+                            (b'CN=Configuration', pyfuse3.ROOT_INODE, cid))
+
+
+    def get_row(self, *a, **kw):
+        self.cursor.execute(*a, **kw)
+        try:
+            row = next(self.cursor)
+        except StopIteration:
+            raise NoSuchRowError()
+        try:
+            next(self.cursor)
+        except StopIteration:
+            pass
+        else:
+            raise NoQniqueValueError()
+
+        return row
+
+
+    async def getattr(self, inode, ctx=None):
+        row = self.get_row("SELECT * FROM inodes WHERE id=?", (inode,))
+
+        entry = pyfuse3.EntryAttributes()
+        entry.st_ino = inode
+        entry.generation = 0
+        entry.entry_timeout = 300
+        entry.attr_timeout = 300
+
+        entry.st_mode = row['mode']
+        entry.st_nlink = self.get_row("SELECT COUNT(inode) FROM contents WHERE inode=?", (inode,))[0]
+        entry.st_uid = row['uid']
+        entry.st_gid = row['gid']
+        entry.st_rdev = row['rdev']
+        entry.st_size = row['size']
+
+        entry.st_blksize = 512
+        entry.st_blocks = 1
+        entry.st_atime_ns = row['atime_ns']
+        entry.st_mtime_ns = row['mtime_ns']
+        entry.st_ctime_ns = row['ctime_ns']
+
+        return entry
+
+    async def lookup(self, inode_p, name, ctx=None):
+        if name == '.':
+            inode = inode_p
+        elif name == '..':
+            inode = self.get_row("SELECT * FROM contents WHERE inode=?", (inode_p,))['parent_inode']
+        else:
+            try:
+                inode = self.get_row("SELECT * FROM contents WHERE name=? AND parent_inode=?", (name, inode_p))['inode']
+            except NoSuchRowError:
+                raise(pyfuse3.FUSEError(errno.ENOENT))
+
+        return await self.getattr(inode, ctx)
+
+
+    async def readlink(self, inode, ctx):
+        return self.get_row("SELECT * FROM inodes WHERE id=?", (inode,))['target']
+
+
+    async def opendir(self, inode, ctx):
+        return inode
+
+
+    async def readdir(self, inode, off, token):
+        if off == 0:
+            off = -1
+
+        self.update_inode(inode)
+        cursor2 = self.db.cursor()
+        cursor2.execute("SELECT * FROM contents WHERE parent_inode=? AND rowid > ? ORDER BY rowid", (inode,off))
+        for row in cursor2:
+            pyfuse3.readdir_reply(token, row['name'], await self.getattr(row['inode']), row['rowid'])
+
+
+    async def open(self, inode, flags, ctx):
+        log.debug('open %s' % inode)
+        return inode
+
+
+    def update_inode_data(self, inode):
+        name = self.get_inode_name(inode)
+        data = None
+        log.debug('updating %s (%s)' % (name, inode))
+        if name == b'.attributes':
+            dn = self.path2dn(self.get_inode_path(inode))[len(name)+1:]
+            data = self.ad.read_node(dn)
+        if data:
+            data_bytes = str(data).encode()
+#            log.debug('data_bytes: %s' % data_bytes.decode('utf-8'))
+            data_ldif = ldif.CreateLDIF('DC=test', dict(data)).encode()
+            self.db.execute("UPDATE inodes SET data=? WHERE id=?", (data_ldif, inode))
+            return len(data_ldif)
+
+        return 0
+
+
+    async def read(self, fh, off, length):
+        log.debug('open %s file (off, length) (%s, %s)' % (fh, off, length))
+        data = self.get_row('SELECT data FROM inodes WHERE id=?', (fh,))[0]
+        if data is None:
+            return b''
+
+        log.debug('data is: %s' % data)
+        return data[off:off+length]
+
+
+def init_logging(debug=False):
+    formatter = logging.Formatter('%(asctime)s.%(msecs)03d %(threadName)s: '
+                                  '[%(name)s] %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    root_logger = logging.getLogger()
+    if debug:
+        handler.setLevel(logging.DEBUG)
+        root_logger.setLevel(logging.DEBUG)
+    else:
+        handler.setLevel(logging.INFO)
+        root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(handler)
+
+def parse_args():
+    '''Parse command line'''
+
+    parser = ArgumentParser()
+
+    parser.add_argument('mountpoint', type=str,
+                        help='Where to mount the file system')
+    parser.add_argument('--debug', action='store_true', default=False,
+                        help='Enable debugging output')
+    parser.add_argument('--debug-fuse', action='store_true', default=False,
+                        help='Enable FUSE debugging output')
+    return parser.parse_args()
+
+
+class NoUniqueValueError(Exception):
+    def __str__(self):
+        return 'Query generated more than 1 result row'
+
+
+class NoSuchRowError(Exception):
+    def __str__(self):
+        return 'Query produced 0 result rows'
+
+def main():
+    options = parse_args()
+    init_logging(options.debug)
+
+    testfs = ADfs()
+    fuse_options = set(pyfuse3.default_options)
+    fuse_options.add('fsname=ad')
+    if options.debug_fuse:
+        fuse_options.add('debug')
+    pyfuse3.init(testfs, options.mountpoint, fuse_options)
+    try:
+        trio.run(pyfuse3.main)
+    except:
+        pyfuse3.close(unmount=False)
+        raise
+
+    pyfuse3.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ adldap.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, sys, errno, logging
+import ldap, ldap.sasl
+from argparse import ArgumentParser
+from logging import debug, info, warning
+# samba imports
+import samba.param
+from samba.credentials import Credentials
+from samba.dcerpc import nbt
+from samba.net import Net
+
+
+log = logging.getLogger(__name__)
+
+def r2dn(r):
+    return ','.join(['DC=%s' % x for x in r.lower().split('.')])
+
+
+class AD(object):
+    def __init__(self, realm):
+        self.realm = realm
+        debug("initialize LDAP")
+        self.__init_samba_params()
+        self.__init_ldap()
+
+    def get(self, node):
+        debug("fetching node: %s" % node)
+        self.get_root()
+
+
+    def __init_ldap(self):
+        pdc = self.finddc()
+        self.l = ldap.initialize('ldap://%s' % (pdc), bytes_mode=False)
+        self.l.procotol_version = 3
+        self.l.set_option(ldap.OPT_X_SASL_NOCANON, True)
+        auth_tokens = ldap.sasl.gssapi('')
+        self.l.sasl_interactive_bind_s('', auth_tokens)
+        self.l.set_option(ldap.OPT_REFERRALS,0)
+
+
+    def get_childs(self, dn):
+        res = self.ldap_search(dn=dn, scope=ldap.SCOPE_ONELEVEL, attrs=['dn'])
+        if not res:
+            return []
+        else:
+            return map(lambda x: x[0], res)
+
+    def get_root(self):
+#        (dn, res) = self.ldap_search(dn=r2dn(realm), scope=ldap.SCOPE_BASE, attrs=['*'])[0]
+        res = self.ldap_search(dn=r2dn(self.realm), scope=ldap.SCOPE_ONELEVEL, attrs=['dn'])
+        return map(lambda x: x[0], res)
+#        res = self.ldap_search(dn=r2dn(realm), scope=ldap.SCOPE_SUBTREE, attrs=['dn'])
+#        for r in res:
+#            (n,_) = r
+#            print(n)
+#        print(res)
+
+
+    def read_node(self, dn):
+        res = self.ldap_search(dn, scope=ldap.SCOPE_BASE, attrs=['*'])[0][1]
+        return res
+
+
+    def ldap_search(self, dn=None, expr=None, scope=ldap.SCOPE_SUBTREE, attrs=[]):
+        if not dn:
+            dn = self.base_dn
+
+        try:
+            if not expr:
+                expr = '(objectClass=*)' # fix for old ldap (should accept None)
+
+            log.debug("ldap_search: (dn, scope, expr, attrs) (%s, %s, %s, %s)" % (dn, scope, expr, attrs))
+            res = self.l.search_s(dn, scope, expr, attrs)
+        except Exception as e:
+            print("search %s on %s failed with %s" % (expr, dn, e))
+            raise
+
+        ret = [ (r, dn) for (r, dn) in res if r ]
+        return ret if ret else None
+
+
+    def finddc(self, realm=None, flags=None):
+        if not realm:
+            realm = self.realm
+
+        if not flags:
+            flags = nbt.NBT_SERVER_LDAP | nbt.NBT_SERVER_DS | nbt.NBT_SERVER_WRITABLE
+
+        net = Net(creds=self.creds, lp=self.lp)
+        ret = net.finddc(domain=realm, flags=flags)
+        return ret.pdc_dns_name
+
+
+    def __init_samba_params(self, root_use_machine_creds = True):
+        lp = samba.param.LoadParm()
+        try:
+            lp.load_default()
+        except Exception as e:
+            raise RuntimeError("cannot load default samba parameters", e)
+
+        creds = Credentials()
+        creds.guess(lp)
+        if os.getuid() == 0 and root_use_machine_creds: # use machine credentials
+            debug('use machine account')
+#            creds.set_machine_account(lp)
+            self.use_machine_creds = True
+
+        self.lp = lp
+        self.realm = lp.get('realm')
+        self.base_dn = r2dn(self.realm)
+        self.creds = creds
+
+
+def init_logging(debug=False):
+    formatter = logging.Formatter('%(asctime)s.%(msecs)03d %(threadName)s: '
+                                  '[%(name)s] %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    root_logger = logging.getLogger()
+    if debug:
+        handler.setLevel(logging.DEBUG)
+        root_logger.setLevel(logging.DEBUG)
+    else:
+        handler.setLevel(logging.INFO)
+        root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(handler)
+
+
+def parse_args():
+    '''Parse command line'''
+
+    parser = ArgumentParser()
+
+    parser.add_argument('--debug', action='store_true', default=False,
+                        help='Enable debugging output')
+    return parser.parse_args()
+
+def main():
+    options = parse_args()
+    init_logging(options.debug)
+
+    ad = AD("domain.alt")
+    ad.get("root")
+
+if __name__ == '__main__':
+    main()
